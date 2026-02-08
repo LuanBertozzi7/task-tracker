780d15976655902dec18f6e9d401a11114a583f0
Luanbertozzi7 <LuanBertozzi7@hotmail.com>
2026-02-08 16:14:40 -0400

    feat: added params for list

--- src/cli/task.list.js
+++ src/cli/task.list.js
@@ -1,14 +1,19 @@
 import { loadTasks } from '../../utilities/tasks.create.js';
 
-export function TaskList(idString) {
-  id = Number(idString);
-
-  if (!id) {
-    const tasks = loadTasks();
-    if (tasks.length === 0) {
-      console.log('Lista vazia.');
-      return;
-    }
-    tasks.forEach((task) => console.log(`ID:[${task.id}] descrição: ${task.description}`));
+export function TaskList(statusString) {
+  const tasks = loadTasks();
+  const tasksTarget = tasks.filter((t) => t.status == statusString) ?? null;
+  
+  // treatment
+  if (tasks.length === 0) {
+    console.log('Lista vazia.');
+    return;
   }
+  
+  if (!statusString) { // returns the entire list
+    tasks.forEach((task) => console.log(`ID:[${task.id}]\n descrição: '${task.description}'\n status: ${task.status}`));
+    return;
+  }
+
+  tasksTarget.forEach((task) => console.log(`ID: [${task.id}]\n descrição: '${task.description}'\n status: ${task.status}`));
 }
