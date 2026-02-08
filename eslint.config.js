461672e0da0ce6d0e8fbd4a9e8d8bbea56e4d169
LuanBertozzi7 <luan@luanbertozzi.luanbertozzi>
2026-02-08 12:45:09 -0400

    added prettier and eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+  {
+    rules: {},
+  },
+];
