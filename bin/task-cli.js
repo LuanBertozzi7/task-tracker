dc5389218f60b5a05197612c6efdcd2127adb64e
Luanbertozzi7 <luan@luanbertozzi.luanbertozzi>
2026-02-08 13:06:47 -0400

    refactor: bin structure

--- bin/task-cli.js
+++ bin/task-cli.js
@@ -1,6 +1,25 @@
-import { taskFileCreate } from "../utilities/tasks.file.create.js";
+/**
+ * @author LuanBertozzi7
+ * @version 0.0.1b
+ */
+
+import { taskFileCreate } from '../utilities/tasks.create.js';
 taskFileCreate();
+import {command, args } from '../src/core/task.args.js';
+// cli functions 
+import { taskAddCli } from '../src/cli/task.add.js';
 
-const args = process.argv.slice(2); // remove node and archive name
-const [command, ...rest] = args;
 
+try {
+  switch (command) {
+    case 'add': {
+      taskAddCli(args);
+      break;
+    }
+    default:
+      console.log('invalid command, use: add | list | update | delete | mark-in-progress');
+  }
+} catch (e) {
+  console.error(String(e.message || e));
+  process.exitCode = 1;
+}
